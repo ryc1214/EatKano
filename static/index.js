b58a696c0cca8fb7a8e838d5c389c405fc8cea9c
ryc1214 <99315163+ryc1214@users.noreply.github.com>
2022-02-09 19:18:36 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '额';
+        if (cps <= 8) return '你是？';
+        if (cps <= 10)  return '硬硬硬';
+        if (cps <= 15) return '666';
+        return '牛逼';
     }
 
     function toStr(obj) {
