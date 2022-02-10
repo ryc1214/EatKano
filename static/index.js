491cc8d91a2b269d8b99d4c4245602ad5f8bb266
ryc1214 <99315163+ryc1214@users.noreply.github.com>
2022-02-10 21:52:43 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,7 +441,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '额';
+        if (cps <= 5) return '呃';
         if (cps <= 8) return '你是？';
         if (cps <= 10)  return '硬硬硬';
         if (cps <= 15) return '666';
